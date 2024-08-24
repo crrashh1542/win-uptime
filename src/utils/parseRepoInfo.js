538badddb7c622072af2173f12182996d7c1cf63
crrashh1542 <kotsuki@crrashh.com>
2024-08-25 01:38:49 +0800

    chore(parser): 移除不需要的项目信息
    经过推敲后认为 Vue 版本和 Fluent 组件库版本号没有显示的必要，故移除
    
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- src/utils/parseRepoInfo.js
+++ src/utils/parseRepoInfo.js
@@ -11,7 +11,6 @@ import buildInfo from '/scripts/buildInfo.json'
 // packageInfo 相关
 const packageVerNum = packageInfo.version // 项目版本号
 const packageRepo = packageInfo.repository.url.split('+')[1] // 项目地址
-const packageDeps = packageInfo.devDependencies // 依赖版本
 
 // buildInfo 相关
 const buildNum = buildInfo.build // 构建次数
@@ -19,15 +18,11 @@ const buildNum = buildInfo.build // 构建次数
 // 数据合并
 const pVersion = 'v' + packageVerNum + ' (build ' + buildNum + ')' // 完整版本
 const pRepoName = packageRepo.split('https://github.com/')[1] // 项目名
-const pVue = 'v' + packageDeps['vue'].split('^')[1] // Vue 框架版本
-const pUI = 'v' + packageDeps['@fluentui/web-components'].split('^')[1] // 组件库版本
 
 // 导出数据
 export default [
-   ['项目版本', 'w-icon-version', pVersion],
+   ['站点版本', 'w-icon-version', pVersion],
    ['项目地址', 'w-icon-github', packageRepo, pRepoName],
-   ['Vue 框架版本', 'w-icon-vuejs', pVue],
-   ['Fluent UI 组件库版本', 'w-icon-fluent', pUI],
    [
       '交流群组', 'w-icon-qq',
       '//qm.qq.com/cgi-bin/qm/qr?k=i3uo_SoY8qYCP-DwlWN0MvBFzU7dIl-V&group_code=442133970',
