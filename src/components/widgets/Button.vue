856ddc38839cdd4c6ca8a5c2358507173ad5afe7
crrashh1542 <kotsuki@crrashh.com>
2024-09-03 18:20:52 +0800

    feat: 添加按钮组件
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

---
+++ src/components/widgets/Button.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'WidgetButton'
+}
+</script>
+
+<template>
+    <span class="button">
+        <slot />
+    </span>
+</template>
+
+<style lang="less">
+@import url('@s/global.less');
+
+.button {
+    display: inline-block;
+    padding: .6em 1em;
+    border: 1px solid @wu-color-border;
+    border-radius: @wu-layout-radius;
+    font-size: 14px;
+    background-color: @wu-color-main;
+    cursor: pointer;
+    transition: background-color .3s;
+}
+.button:hover {
+    background-color: @wu-color-base;
+}
+</style>
